784ef21ab36b915746028c47b67f4881ae514ae7
anton_davidovich <dvd14@ukr.net>
2019-02-17 16:37:28 +0200

    Solved

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const fse = require('fs-extra');
+const fs = require('fs-extra');
 const needle = require('needle');
 
-needle('get', 'https://swapi.co/api/starships/')
+needle('get', 'https://swapi.co/api/planets/8/')
   .then(function(resp) {
-    // ...
-    
+    const filePath = JSON.stringify(resp.body);
+    return filePath;
+  })
+  .then(function(filePath){
+    fs.writeFile('/home/kavabata/proCode/Javascript/lesson-10-swapi/swapiData.txt', filePath, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
   })
   .catch(function(err) {
-    // ...
-  })
-});
+    console.log('error');
+  });
